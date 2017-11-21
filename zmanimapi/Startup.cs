b677f362ac72c3d27b0dd5a25dc80a2944cced93
GreatJack <wyre@gmail.com>
2017-11-21 13:57:01 -0500

    enabled cors api wide

--- zmanimapi/Startup.cs
+++ zmanimapi/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +24,8 @@ namespace zmanimapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //add cors so clients can consume the api with a static page
+            services.AddCors();
             services.AddMvc();
         }
 
@@ -33,6 +36,9 @@ namespace zmanimapi
             {
                 app.UseDeveloperExceptionPage();
             }
+            //enable cors for all requests to allow cross domain acsess to the zemanim api
+            app.UseCors(builder =>
+     builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             //enable static files for the help page that will be routed through the controller
             app.UseStaticFiles();
             app.UseMvc(routes =>
